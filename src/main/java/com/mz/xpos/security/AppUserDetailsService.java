64058483cda1498020493e0301161e35d5a2be0d
fxavier <xavierfrancisco353@gmail.com>
2015-08-02 09:50:43 +0200

    Security added

---
+++ src/main/java/com/mz/xpos/security/AppUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.mz.xpos.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.mz.xpos.dao.Usuarios;
+import com.mz.xpos.model.Grupo;
+import com.mz.xpos.model.Usuario;
+import com.mz.xpos.util.cdi.CDIServiceLocator;
+
+
+public class AppUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Usuarios usuarios = CDIServiceLocator.getBean(Usuarios.class);
+		Usuario usuario = usuarios.porEmail(email);
+		
+		UsuarioSistema user = null;
+		
+		if (usuario != null) {
+			user = new UsuarioSistema(usuario, getGrupos(usuario));
+		}
+		
+		return user;
+	}
+
+	private Collection<? extends GrantedAuthority> getGrupos(Usuario usuario) {
+		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		
+		for (Grupo grupo : usuario.getGrupos()) {
+			authorities.add(new SimpleGrantedAuthority(grupo.getNome().toUpperCase()));
+		}
+		
+		return authorities;
+	}
+
+}
